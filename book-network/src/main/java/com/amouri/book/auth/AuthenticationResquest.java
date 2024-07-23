93e549c154bc20f7c4a0a8902d9923ed3049e102
CyberCodeCracker <souhailamouri123@gmail.com>
2024-07-23 21:53:18 +0100

    Added exception handling for locked account, disabled account, bad credentials, messaging exception, invalid method args, and any other exception handled by Exception.class

--- book-network/src/main/java/com/amouri/book/auth/AuthenticationResquest.java
+++ book-network/src/main/java/com/amouri/book/auth/AuthenticationResquest.java
@@ -2,6 +2,7 @@ package com.amouri.book.auth;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 import lombok.Builder;
 import lombok.Getter;
@@ -13,8 +14,10 @@ import lombok.Setter;
 public class AuthenticationResquest {
 
     @Email(message = "Email is not formatted correctly")
+    @NotEmpty(message = "Email is required")
     @NotBlank(message = "Email is required")
     private String email;
+    @NotEmpty(message = "Password is required")
     @NotBlank(message = "Password is required")
     @Size(min = 8, message = "Password should be 8 characters at least")
     private String password;
---
+++ book-network/src/main/java/com/amouri/book/handler/GlobalExceptionHandler.java
@@ -0,0 +1,105 @@
+package com.amouri.book.handler;
+
+import jakarta.mail.MessagingException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
+import org.springframework.security.authentication.LockedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    // Handles LockedException (locked account) by returning an HTTP 401 Unauthorized response with error details.
+    @ExceptionHandler(LockedException.class)
+    public ResponseEntity<ExceptionResponse> handleException(LockedException exp) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(
+                        ExceptionResponse.builder()
+                                .businessErrorCode(BusinessErrorCodes.ACCOUNT_LOCKED.getCode())
+                                .businessErrorDescription(BusinessErrorCodes.ACCOUNT_LOCKED.getDescription())
+                                .error(exp.getMessage())
+                                .build()
+                );
+    }
+
+    // Handles DisabledException (disabled account) by returning an HTTP 401 Unauthorized response with error details.
+    @ExceptionHandler(DisabledException.class)
+    public ResponseEntity<ExceptionResponse> handleException(DisabledException exp) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(
+                        ExceptionResponse.builder()
+                                .businessErrorCode(BusinessErrorCodes.ACCOUNT_DISABLE.getCode())
+                                .businessErrorDescription(BusinessErrorCodes.ACCOUNT_DISABLE.getDescription())
+                                .error(exp.getMessage())
+                                .build()
+                );
+    }
+
+    // Handles BadCredentialsException (wrong login credentials) by returning an HTTP 401 Unauthorized response with error details.
+    @ExceptionHandler(BadCredentialsException.class)
+    public ResponseEntity<ExceptionResponse> handleException(BadCredentialsException exp) {
+        return ResponseEntity
+                .status(HttpStatus.UNAUTHORIZED)
+                .body(
+                        ExceptionResponse.builder()
+                                .businessErrorCode(BusinessErrorCodes.BAD_CREDENTIALS.getCode())
+                                .businessErrorDescription(BusinessErrorCodes.BAD_CREDENTIALS.getDescription())
+                                .error(BusinessErrorCodes.BAD_CREDENTIALS.getDescription())
+                                .build()
+                );
+    }
+
+    // Handles MessagingException (Issues with sending email) by returning an HTTP 500 Internal Server Error response with error details.
+    @ExceptionHandler(MessagingException.class)
+    public ResponseEntity<ExceptionResponse> handleException(MessagingException exp) {
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(
+                        ExceptionResponse.builder()
+                                .error(exp.getMessage())
+                                .build()
+                );
+    }
+
+    // Handles MethodArgumentNotValidException by returning an HTTP 400 Bad Request response with validation error details.
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ExceptionResponse> handleException(MethodArgumentNotValidException exp) {
+        Set<String> errors = new HashSet<>();
+        exp.getBindingResult().getAllErrors()
+                .forEach(error -> {
+                    var errorMessage = error.getDefaultMessage();
+                    errors.add(errorMessage);
+                });
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(
+                        ExceptionResponse.builder()
+                                .validationErrors(errors)
+                                .build()
+                );
+    }
+
+    // Handles any other Exception by returning an HTTP 500 Internal Server Error response with a generic error message.
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleException(Exception exp) {
+        // log the exception
+        exp.printStackTrace();
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(
+                        ExceptionResponse.builder()
+                                .businessErrorDescription("Internal error, contact the admin")
+                                .error(exp.getMessage())
+                                .build()
+                );
+    }
+}
