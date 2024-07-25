99b0c9c1675ec421521230c7ca75ae8094065292
CyberCodeCracker <souhailamouri123@gmail.com>
2024-07-25 13:39:12 +0100

    Implemented findBookById method.

--- book-network/src/main/java/com/amouri/book/book/BookController.java
+++ book-network/src/main/java/com/amouri/book/book/BookController.java
@@ -1,14 +1,12 @@
 package com.amouri.book.book;
 
+import com.amouri.book.common.PageResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("books")
@@ -18,11 +16,29 @@ public class BookController {
 
     private final BookService service;
 
+    // Saves a book in the database
     @PostMapping
     public ResponseEntity<Integer> saveBook(
             @Valid @RequestBody BookRequest request,
             Authentication connectedUser
     ) {
         return ResponseEntity.ok(service.save(request, connectedUser));
+    }
+
+    // Returns one book by its ID
+    @GetMapping("{book-id}")
+    public ResponseEntity<BookResponse> findBookById(
+          @PathVariable("book-id") Integer bookId
+    ) {
+        return ResponseEntity.ok(service.findById(bookId));
+    }
+
+    @GetMapping
+    public ResponseEntity<PageResponse<BookResponse>> findAllBooks (
+            @RequestParam(name = "page", defaultValue = "0", required = false) int page,
+            @RequestParam(name = "size", defaultValue = "10", required = false) int size,
+            Authentication connectedUser
+    ) {
+        return ResponseEntity.ok(service.findAllBooks(page, size, connectedUser));
     }
 }
