1a55f808f2489a23cc717d977a803871398afa90
CyberCodeCracker <souhailamouri123@gmail.com>
2024-07-28 10:11:44 +0100

    Implemented save book cover picture feature

--- book-network/src/main/java/com/amouri/book/book/BookService.java
+++ book-network/src/main/java/com/amouri/book/book/BookService.java
@@ -2,6 +2,7 @@ package com.amouri.book.book;
 
 import com.amouri.book.common.PageResponse;
 import com.amouri.book.exception.OperationNotPermittedException;
+import com.amouri.book.file.FileStorageService;
 import com.amouri.book.history.BookTransactionHistory;
 import com.amouri.book.history.BookTransactionHistoryRepository;
 import com.amouri.book.user.User;
@@ -13,6 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Objects;
@@ -24,6 +26,7 @@ public class BookService {
     private final bookRepository bookRepository;
     private final BookMapper bookMapper;
     private final BookTransactionHistoryRepository bookTransactionHistoryRepository;
+    private final FileStorageService fileStorageService;
     public Integer save(BookRequest request, Authentication connectedUser) {
         User user = ((User) connectedUser.getPrincipal());
         Book book = bookMapper.toBook(request);
@@ -133,7 +136,7 @@ public class BookService {
         return bookId;
     }
 
-    public Integer burrowBook(Integer bookId, Authentication connectedUser) {
+    public Integer borrowBook(Integer bookId, Authentication connectedUser) {
         Book book = bookRepository.findById(bookId)
                 .orElseThrow(() -> new EntityNotFoundException("Book with ID:: " + bookId + " not found"));
         if(book.isArchived() || !book.isShareable()) {
@@ -186,5 +189,14 @@ public class BookService {
                 .orElseThrow(() -> new OperationNotPermittedException("You did not borrow this book"));
         bookTransactionHistory.setReturnApproved(true);
         return bookTransactionHistoryRepository.save(bookTransactionHistory).getId();
+    }
+
+    public void uploadBookCoverPicture(MultipartFile file, Authentication connectedUser, Integer bookId) {
+        Book book = bookRepository.findById(bookId)
+                .orElseThrow(() -> new EntityNotFoundException("Book with ID:: " + bookId + " not found"));
+        User user = ((User) connectedUser.getPrincipal());
+        var bookCover = fileStorageService.saveFile(file, user.getId());
+        book.setBookCover(bookCover);
+        bookRepository.save(book);
     }
 }
---
+++ book-network/src/main/java/com/amouri/book/file/FileStorageService.java
@@ -0,0 +1,70 @@
+package com.amouri.book.file;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import static java.io.File.separator;
+import static java.lang.System.currentTimeMillis;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class FileStorageService {
+
+    @Value("${application.file.upload.photos-output-path}")
+    private String fileUploadPath;
+
+    public String saveFile(
+            @NonNull MultipartFile sourceFile,
+            @NonNull Integer userId) {
+        final String fileUploadSubPath = "users" + separator + userId;
+        return uploadFile(sourceFile, fileUploadSubPath);
+    }
+
+    private String uploadFile(
+            @NonNull MultipartFile sourceFile,
+            @NonNull String fileUploadSubPath) {
+        final String finalUploadPath = fileUploadPath + separator + fileUploadSubPath;
+        File targetFolder = new File(finalUploadPath);
+        if (!targetFolder.exists()) {
+            boolean folerCreated = targetFolder.mkdirs();
+            if (!folerCreated) {
+                log.warn("Failed to create the target folder");
+                return null;
+            }
+        }
+        final String fileExtension = getFileExtension(sourceFile.getOriginalFilename());
+        // Location of saved file e.g. ./upload/users/1/5323313374.jpg
+        String targetFilePath = fileUploadPath + separator + currentTimeMillis() + "." + fileExtension;
+        Path targetPath = Paths.get(targetFilePath);
+        try {
+            Files.write(targetPath, sourceFile.getBytes());
+            log.info("File saved to " + targetFilePath);
+            return targetFilePath;
+        } catch (IOException e) {
+            log.error("File was not save", e);
+        }
+        return null;
+    }
+
+    private String getFileExtension(String originalFilename) {
+        if (originalFilename == null || originalFilename.isEmpty()) {
+            return "";
+        }
+        int lastDotIndex = originalFilename.lastIndexOf(".");
+        if (lastDotIndex == -1) {
+            return  "";
+        }
+        return originalFilename.substring(lastDotIndex + 1).toLowerCase();
+    }
+}
